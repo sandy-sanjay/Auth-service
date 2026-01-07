6ccfa689f59cc1b1a2181951965e49e957a9a80f
sandy-sanjay <sandysanjay589@gmail.com>
2026-01-07 14:44:12 +0530

    Disable DataInitializer in prod profile

--- src/main/java/com/ordernexus/authservice/config/DataInitializer.java
+++ src/main/java/com/ordernexus/authservice/config/DataInitializer.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@Profile("dev")
 @RequiredArgsConstructor
 public class DataInitializer {
 
